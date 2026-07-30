e88ad5800979cbe74a170368be72baa79288e95e
sujit1661 <sujit166100@gmail.com>
2026-07-30 11:34:08 +0530

    added compactibility with groq

--- crewai/main.py
+++ crewai/main.py
@@ -1,61 +1,46 @@
-from crewai import Crew,Agent,Task,Process
+from crewai import Agent, Task, Crew, Process, LLM
 from dotenv import load_dotenv
-from langchain_groq import ChatGroq
 
 load_dotenv()
-llm_1 = ChatGroq(
-    model="openai/gpt-oss-20b",
-    temperature=0.3
+
+llm = LLM(
+    model="groq/openai/gpt-oss-20b",
+    temperature=0.3,
 )
 
-llm_2 = ChatGroq(
-    model="openai/gpt-oss-20b",
-    temperature=0.3
+researcher = Agent(
+    role="Researcher",
+    goal="Research the given topic",
+    backstory="Expert researcher",
+    llm=llm,
+    verbose=True,
 )
 
-
-
-researcher=Agent(
-    role="researcher",
-    goal="Find accurate and latest information on a topic",
-    backstory="Expert in research and data gathering",
-    llm=llm_1,
-    verbose=True
+writer = Agent(
+    role="Writer",
+    goal="Write a blog",
+    backstory="Professional content writer",
+    llm=llm,
+    verbose=True,
 )
 
-
-writer=Agent(
-    role="writer",
-    goal="Write engaging and structured blog content",
-    backstory="Professional blog writer",
-    llm=llm_2,
-    verbose=True
-)
-
-
-topic=input("topic: ")
-
-task1=Task(
-    description=f"Research the topic: {topic}. Find key points, facts, and trends",
-    expected_output="bullet points structure",
+task1 = Task(
+    description="Research AI Agents",
+    expected_output="Bullet points",
     agent=researcher,
 )
 
 task2 = Task(
-    description=f"Write a detailed blog on: {topic} using the research.",
-    expected_output="Well-structured blog with headings",
-    agent=writer
+    description="Write a blog using the research.",
+    expected_output="A complete blog",
+    agent=writer,
 )
 
-
-crew=Crew(
-    agents=[researcher,writer],
+crew = Crew(
+    agents=[researcher, writer],
     tasks=[task1, task2],
     process=Process.sequential,
-    verbose=True
 )
 
-
-result=crew.run()
-print("\n\n🔥 FINAL OUTPUT:\n")
+result = crew.kickoff()
 print(result)
